733ddec96fca7ff69da1418aa9a135475902911a
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-01-09 23:24:28 -0800

    Add: New Advanced level

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,76 +1,93 @@
-let cards = [
+let cardCollection = [
     {
         rank: "paris",
-        display: false,
         cardImage: "images/paris.png",
         id: 1
     },
-
     {
         rank: "paris",
-        display: false,
         cardImage: "images/paris.png",
         id: 2
     },
-
     {
         rank: "london",
-        display: false,
         cardImage: "images/london.png",
         id: 3
     },
-
     {
         rank: "london",
-        display: false,
         cardImage: "images/london.png",
         id: 4
     },
-
     {
         rank: "new_york",
-        display: false,
         cardImage: "images/new_york.png",
         id: 5
     },
-
     {
         rank: "new_york",
-        display: false,
         cardImage: "images/new_york.png",
         id: 6
+    },
+    {
+        rank: "japan",
+        cardImage: "images/japan.png",
+        id: 7
+    },
+    {
+        rank: "japan",
+        cardImage: "images/japan.png",
+        id: 8
+    },
+    {
+        rank: "amsterdam",
+        cardImage: "images/amsterdam.png",
+        id: 9
+    },
+    {
+        rank: "amsterdam",
+        cardImage: "images/amsterdam.png",
+        id: 10
+    },
+    {
+        rank: "italy",
+        cardImage: "images/italy.png",
+        id: 11
+    },
+    {
+        rank: "italy",
+        cardImage: "images/italy.png",
+        id: 12
     }
 ];
+let cards = [];
 
-let shuffledCards = shuffle(cards);
-let cardsInPlay = [];
-
-//Start the game
-function game() {
-    createNewBoard();
-    foundMatches = 0;
-}
-
-//Create HTML images
-function createNewBoard() {
-    for (let i = 0; i < shuffledCards.length; i += 1) {
-        let cardElement = document.createElement("img");
-        cardElement.setAttribute('cardIndex', i);
-        cardElement.className += "cardImage";
-        cardElement.addEventListener('click', showCard);
-        document.getElementById('game-board').appendChild(cardElement);
-    };
+//Sets the length of cards array according to level
+function createGameSize(length) {
+    resetBoard();
+    cards = [];
+    for (let i = 0; i < length; i += 1) {
+        let card = cardCollection[i];
+        cards.push(card);
+    }
+    cards = shuffle(cards);
 
     resetBoard();
+};
+
+let cardsInPlay = [];
+
+//Create HTML images
+function createDefaultBoard() {
+    createGameSize(6);
 };
 
 //Show card face
 function showCard() {
     let cardId = this.getAttribute('cardIndex');
-    cardsInPlay.push(shuffledCards[cardId]);
-    shuffledCards[cardId].seen = true;
-    this.setAttribute('src', shuffledCards[cardId].cardImage);
-    
+    cardsInPlay.push(cards[cardId]);
+    this.setAttribute('src', cards[cardId].cardImage);
+
     if (cardsInPlay.length === 2) {
         checkForMatch();
     };
@@ -78,14 +95,14 @@ function showCard() {
 
 //Check if 2 cards match
 function checkForMatch() {
-    if (cardsInPlay[0].id !== cardsInPlay[1].id && 
+    if (cardsInPlay[0].id !== cardsInPlay[1].id &&
         cardsInPlay[0].rank === cardsInPlay[1].rank) {
-            cardsInPlay = [];
+        cardsInPlay = [];
     } else if (cardsInPlay[0].id === cardsInPlay[1].id) {
         alert("HA! It's the same card! Try again!");
         resetBoard();
     } else {
-        setTimeout(function(){ resetBoard(); }, 700);
+        setTimeout(function () { resetBoard(); }, 700);
     }
 }
 
@@ -95,32 +112,62 @@ function resetBoard() {
     let allCards = document.querySelectorAll('.cardImage');
     for (let i = 0; i < allCards.length; i += 1) {
         let card = allCards[i];
-        card.setAttribute('src', "images/back.png");
+        card.parentNode.removeChild(card);
+    };
+
+    for (let i = 0; i < cards.length; i += 1) {
+        let cardElement = document.createElement("img");
+        cardElement.setAttribute('cardIndex', i);
+        cardElement.className += "cardImage";
+        cardElement.addEventListener('click', showCard);
+        document.getElementById('game-board').appendChild(cardElement);
+        cardElement.setAttribute('src', "images/back.png")
     };
 };
 
 //Refresh the game and shuffle
 function resetButtonClicked() {
     resetBoard();
-    shuffledCards = shuffle(cards);
+    cards = shuffle(cards);
 };
 
 //Shuffle all cards
 function shuffle(array) {
-	var currentIndex = array.length;
-	var temporaryValue, randomIndex;
+    var currentIndex = array.length;
+    var temporaryValue, randomIndex;
 
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	};
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    };
 
-	return array;
+    return array;
 };
 
-game();
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function showDropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
+
+createDefaultBoard();
 
