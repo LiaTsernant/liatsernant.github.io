4ca2a83c2ecd08dabe4e7024aca94e5effd34511
LiaTsernant <yulia.tsernant@d-d.me>
2020-02-19 21:40:25 -0800

    Add giffaw project

---
+++ giffaw/scripts/app.js
@@ -0,0 +1,34 @@
+let gallery = document.querySelector('.gif-gallery');
+
+function setupQueryValue() {
+    let currentParams = new URLSearchParams(window.location.search)
+    let currentQ = currentParams.get('q');
+    if (currentQ === null) {
+        currentQ = "cats"
+    };
+    document.querySelector('.gif-input').setAttribute("value", currentQ);
+};
+setupQueryValue();
+
+$.ajax({
+    method: 'GET',
+    url: 'http://api.giphy.com/v1/gifs/search',
+    data: $('form').serialize(),
+    success: onSuccess,
+    error: onError
+});
+
+function onSuccess(json) {
+    json.data.forEach((imageData) => {
+        let img = document.createElement('img');
+        img.setAttribute('src', imageData.images.downsized.url);
+        gallery.appendChild(img);
+    });
+};
+
+function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+};
